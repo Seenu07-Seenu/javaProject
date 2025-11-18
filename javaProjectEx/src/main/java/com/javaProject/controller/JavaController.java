b6d9318d49521d5f231b133584b3b98ca43d1fcc
Seenu07-Seenu <srinivasanpalanikumar07@gmail.com>
2025-11-18 16:22:38 +0530

    Third commit

--- javaProjectEx/src/main/java/com/javaProject/controller/JavaController.java
+++ javaProjectEx/src/main/java/com/javaProject/controller/JavaController.java
@@ -1,7 +1,9 @@
 package com.javaProject.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.support.Repositories;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,22 +13,40 @@ import org.springframework.web.bind.annotation.RestController;
 import com.javaProject.service.JavaService;
 
 @RestController
-@RequestMapping("/java")	
+@RequestMapping("/java")
 public class JavaController {
 
 	@Autowired
 	private JavaService javaService;
-	
+
 	@PostMapping("/reversing")
-	public ResponseEntity<String> reversingString(@RequestParam String val){
+	public ResponseEntity<String> reversingString(@RequestParam String val) {
 		ResponseEntity<String> rev = ResponseEntity.ok(javaService.reversingString(val));
 		return rev;
 	}
-	
-	
+
 	@PostMapping("/palindrome")
-	public ResponseEntity<String> palindromeString(@RequestParam String val){
+	public ResponseEntity<String> palindromeString(@RequestParam String val) {
 		String rev = javaService.reversingString(val);
 		return ResponseEntity.ok(javaService.palindromeString(val, rev));
+	}
+
+	@PostMapping("/checkPrime")
+	public ResponseEntity<Map<String, List<Integer>>> isPrime(@RequestParam Integer[] val) {
+		Map<String, List<Integer>> arr = javaService.isPrime(val);
+		return ResponseEntity.ok(arr);
+	}
+
+	@PostMapping("/checkAnagram")
+	public ResponseEntity<String> isAnagram(@RequestParam String val1,@RequestParam String val2){
+		String anagram = javaService.isAnagram(val1,val2);
+		return ResponseEntity.ok(anagram);
+	}
+	
+	@PostMapping("/checkArmstrong")
+	public ResponseEntity<String> isArmstrong(@RequestParam int val){
+		String arm = javaService.isArmstrong(val);
+		return ResponseEntity.ok(arm);
+				
 	}
 }
--- javaProjectEx/src/main/java/com/javaProject/service/JavaService.java
+++ javaProjectEx/src/main/java/com/javaProject/service/JavaService.java
@@ -1,5 +1,11 @@
 package com.javaProject.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +20,7 @@ public class JavaService {
 	@Autowired
 	private JavaRepository javaRepository;
 
-	//Reverse A String
+	// Reverse A String
 	public String reversingString(String val) {
 		Detailes det = new Detailes();
 		StringBuilder rev = new StringBuilder(val).reverse();
@@ -28,13 +34,13 @@ public class JavaService {
 		return String.valueOf(rev);
 	}
 
-	//palindrome
+	// palindrome
 	@Transactional
 	public String palindromeString(String val, String rev) {
 
 		Detailes det = new Detailes();
 		String out;
-		
+
 		if (val.equals(rev)) {
 			out = val + " is palindrome";
 		} else {
@@ -47,6 +53,97 @@ public class JavaService {
 
 		javaRepository.save(det);
 		return out;
+	}
+
+	// chack prime number
+	public Map<String, List<Integer>> isPrime(Integer[] val) {
+		Detailes det = new Detailes();
+		List<Integer> prime = new ArrayList<>();
+		List<Integer> notPrime = new ArrayList<>();
+		Map<String, List<Integer>> primeVal = new HashMap<>();
+
+		for (int i = 0; i < val.length; i++) {
+			if (val[i] == 2) {
+				prime.add(val[i]);
+			} else if (val[i] % 2 == 0 || val[i] % 3 == 0 || val[i] % 5 == 0) {
+				notPrime.add(val[i]);
+			} else {
+				prime.add(val[i]);
+			}
+		}
+		primeVal.put("prime", prime);
+		primeVal.put("not Prime", notPrime);
+
+		det.setInput(Arrays.toString(val));
+		det.setOutput(String.valueOf(primeVal));
+		det.setProgram_name("find prime number");
+		javaRepository.save(det);
+		return primeVal;
+	}
+
+	// find a anagram
+	public  String isAnagram(String val1, String val2) {
+		Detailes det = new Detailes();
+		String sort1 = isSort(val1);
+		String sort2 = isSort(val2);
+		
+		String out = "";
+		
+		if(sort1.equals(sort2)) {
+			out =  "is Anagram";
+		}else {
+			out = "is not Anagram";
+		}
+		
+		det.setInput(val1+", "+val2);
+		det.setOutput(out);
+		det.setProgram_name("Anagram");
+		javaRepository.save(det);
+		return val1+" "+out+" "+val2;
+	}
+
+	// sort a String
+	public static String isSort(String val) {
+		char[] arr = val.toCharArray();
+
+		for (int i = 0; i < arr.length; i++) {
+			int minTemp = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[minTemp]) {
+					minTemp = j;
+				}
+			}
+			char temp = arr[i];
+			arr[i] = arr[minTemp];
+			arr[minTemp] = temp;
+		}
+		return new String(arr);
+	}
+
+	//Armstrong Number
+	public String isArmstrong(int val) {
+		
+		int in = val;
+		String output = "";
+		int out = 0;
+		while(val>0) {
+			int temp = val%10;
+			out += temp*temp*temp;
+			val = val/10;
+		}
+		
+		if(in == out) {
+			output = in+" is Armstrong Number "+ out;
+		}else {
+			output = in+" is Not Armstrong Number "+ out;
+		}
+		Detailes det = new Detailes();
+		det.setInput(String.valueOf(in));
+		det.setOutput(String.valueOf(out));
+		det.setProgram_name("ArmStrong");
+		javaRepository.save(det);
+		
+		return output;
 	}
 
 }
